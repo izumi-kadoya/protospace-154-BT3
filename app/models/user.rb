70b12ac44705ffba4edc754c963bad62428db87b
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-18 09:22:49 +0900

    ユーザーバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, :profile, :job, :position, presence: true
+
 
          
 end
