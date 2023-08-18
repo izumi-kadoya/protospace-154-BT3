2f09e21094011b71f079c365d65a4c85b076ab24
hatomugi21 <hatomugi.21@gmail.com>
2023-08-18 13:03:55 +0900

    実装中１

--- db/migrate/20230817102909_create_prototypes.rb
+++ db/migrate/20230817102909_create_prototypes.rb
@@ -1,7 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.
+      t.string :protoname, null: false
+      t.string :catchcopy, null: false
+      t.text :concept, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
